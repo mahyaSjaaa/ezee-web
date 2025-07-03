839107e507e0a6b5890db75367351d34a149f4dd
raffimahya <raffimlbb.23@gmail.com>
2025-07-03 23:30:10 +0700

    header

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,44 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    const securityHeaders = [
+      {
+        key: "Content-Security-Policy",
+        value: `
+          default-src 'self';
+          script-src 'self' 'unsafe-inline' 'unsafe-eval' *.vercel.app;
+          style-src 'self' 'unsafe-inline' fonts.googleapis.com;
+          font-src 'self' fonts.gstatic.com data:;
+          img-src 'self' data: blob:;
+          connect-src *;
+        `.replace(/\n/g, ""),
+      },
+      {
+        key: "X-Frame-Options",
+        value: "SAMEORIGIN",
+      },
+      {
+        key: "X-Content-Type-Options",
+        value: "nosniff",
+      },
+      {
+        key: "Referrer-Policy",
+        value: "strict-origin-when-cross-origin",
+      },
+      {
+        key: "Permissions-Policy",
+        value: "geolocation=(), microphone=(), camera=()",
+      },
+    ];
+
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
